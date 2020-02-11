d0c7738ee3c1ad49b88596d94d0887c547f60b84
Ganesh Prasad Rai <Ganesh Prasad Rai@LAPTOP-BD1UD23M>
2020-02-11 19:45:39 +0530

    First commit.....

---
+++ src/test/java/com/srdt/SrdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
